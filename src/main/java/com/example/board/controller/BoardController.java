70b8e40f44f4bcb7ad0f67ffd6f3614004e0f5b0
양한동 <yhd0121@gs.anyang.ac.kr>
2024-11-21 11:41:19 +0900

    first commit

---
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -0,0 +1,8 @@
+package com.example.board.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BoardController {
+}
---
+++ src/test/java/com/example/board/Board01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
